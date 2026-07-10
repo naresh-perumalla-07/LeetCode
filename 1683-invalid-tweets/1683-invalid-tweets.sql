7dd08cb52ddaedb12a3170e4c901f82aa6804bef
Perumalla Naresh <naresh.xplores777@gmail.com>
2026-07-10 15:50:59 +0530

    Time: 567 ms (95.69%), Space: 0B (100%) - LeetHub

---
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT tweet_id FROM Tweets WHERE LENGTH(content)>15;
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
