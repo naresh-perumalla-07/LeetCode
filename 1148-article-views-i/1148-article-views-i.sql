9c6fc8e06ca9a7dae34891fd5e8a897a20359084
Perumalla Naresh <naresh.xplores777@gmail.com>
2026-07-10 15:48:40 +0530

    Time: 427 ms (62.16%), Space: 0B (100%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT DISTINCT author_id AS id FROM Views WHERE author_id=viewer_id ORDER BY author_id;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
